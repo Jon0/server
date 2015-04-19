09d94102a1b74f9955acc37846e3a2ed4740651b
Jon0 <Jono4728@gmail.com>
2015-04-19 18:36:45 +1200

    more output

--- src/system/system.cpp
+++ src/system/system.cpp
@@ -20,7 +20,9 @@ PowerCtrl *System::get_power_ctrl() {
 }
 
 std::string System::html() {
-	std::string idle_time = std::to_string(pwr.idle_seconds().count() / 60) + " minutes";
+	int idle_time = (int)(pwr.idle_seconds().count() / 60);
+	std::string idle_time_str = std::to_string(idle_time) + " minutes";
+	std::string remain_str = std::to_string(120 - idle_time) + " minutes";
 	std::string str;
 	str += "<!DOCTYPE html>";
 	str += "<html>";
@@ -28,7 +30,8 @@ std::string System::html() {
 	str += "<title>test</title>";
 	str += "</head>";
 	str += "<body>";
-	str += "<h1>system idle time "+idle_time+"</h1>";
+	str += "<h1>system idle time "+idle_time_str+"</h1>";
+	str += "<h1>shutdown in "+remain_str+"</h1>";
 	str += "<a href=\"/off\">turn off</a>";
 	str += "</body>";
 	str += "</html>";
